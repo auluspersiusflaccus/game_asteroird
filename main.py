3c917ff65c753a57f5e8bbe148788c6804c9c264
Auluspersiusflaccus <felix.charron-ducharme.1@ulaval.ca>
2024-10-02 13:00:13 -0400

    D: ajusted frame-rate to 60

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import pygame
 from constants import *
 def main():
 	pygame.init()
+	clock=pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
 		pygame.Surface.fill(screen, (0,0,0))
 		pygame.display.flip()
-
+		clock.tick(60)
+		dt = clock.tick(60) / 1000
 
 	
 
